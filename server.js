4afca1ef4547a6dc0a20a494f99d76b2403db0c4
jamestang12 <homing.tang@ryerson.ca>
2021-06-20 16:39:25 -0400

    connect server to RDS

--- server.js
+++ server.js
@@ -1,16 +1,25 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 require('dotenv').config();
-
+const sql = require("./config/db")
 const app = express();
+
+sql.connect(function(err) {
+    if (err) {
+      console.error('Database connection failed: ' + err.stack);
+      return;
+    }
+  
+    console.log('Connected to database.');
+  });
+
+sql.end();
+
+app.use(bodyParser.json());
 
 const PORT = process.env.PORT || 5000;
 
 app.get('/test',(req,res) => res.status(200).send(`Hello World`))
-
-
-
-
-
 
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
